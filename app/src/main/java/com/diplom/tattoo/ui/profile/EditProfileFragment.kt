611827d5e6692ffb1974413d1e1169756b0b7658
Булат Баржанов <b.barzhanov@yandex.ru>
2022-05-01 19:28:46 +0300

    Доделал навигацию в профиле

--- app/src/main/java/com/diplom/tattoo/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/diplom/tattoo/ui/profile/EditProfileFragment.kt
@@ -5,19 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.diplom.tattoo.R
+import com.diplom.tattoo.databinding.FragmentEditProfileBinding
+import com.diplom.tattoo.databinding.FragmentRegBinding
 
 class EditProfileFragment : Fragment() {
+    private var _binding: FragmentEditProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        btnListeners()
+
+        return binding.root
     }
 
-    companion object {
+    private fun btnListeners() {
 
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
